b3cee6f296cdb3bb2359511fdac09cb8c8e21fd6
Renan Castro <renancruz.castro@gmail.com>
2021-03-11 15:36:18 -0300

    merge

---
+++ aula_4.py
@@ -0,0 +1,15 @@
+# Aula de For
+
+print("Após digitar um número, o programa irá informar se o número é primo!")
+a = int(input("Digite um número: "))
+for a in range (0, a + 1):
+    div = 0
+    for x in range(1, a + 1):
+        resto = a % x
+        if resto == 0:
+            div += 1
+if div == 2:
+    print("{} é um número primo.".format(a))
+else:
+    print("{} não é um número primo.".format(a))
+print("Final do Programa")
