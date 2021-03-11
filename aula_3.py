003540ae7e1992110b87c5b8260c0e6f0d8f6a8a
Renan Castro <renancruz.castro@gmail.com>
2021-03-10 22:24:32 -0300

    commit aula 3

---
+++ aula_3.py
@@ -0,0 +1,18 @@
+# Programa que me retorna se há um número par nas variáveis inseridas
+
+
+a = int(input("Entre com o primeiro valor: "))
+b = int(input("Entre com o segundo valor: "))
+restoa = a % 2
+restob = b % 2
+if restoa == 0 and restob == 0:
+    print("Ambos os números são pares")
+elif restoa == 0 or restob == 0:
+    print("Foi digitado pelo menos um número par")
+else:
+    print("Não foi digitado número par")
+print("Fim do programa")
+
+
+
+
